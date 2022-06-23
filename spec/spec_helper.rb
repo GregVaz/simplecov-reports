d786d6c0d1d1b39cb31bfeb50bcf680f0ff22f3e
Gregorio Vazquez <gregoriovazya@gmail.com>
2022-06-23 15:35:44 -0600

    fix: add enable_coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 if ENV['COVERAGE'] == 'true'
 	require 'simplecov'
 	SimpleCov.start do
+		enable_coverage :branch
 		minimum_coverage line: 90, branch: 80
 		maximum_coverage_drop line: 5, branch: 10
 	end
