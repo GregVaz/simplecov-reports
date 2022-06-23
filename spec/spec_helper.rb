3aa37442dc5d640fb62b76679531227df5902cd6
Gregorio Vazquez <gregoriovazya@gmail.com>
2022-06-23 15:58:34 -0600

    feat: new workflow

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,16 @@
 
 if ENV['COVERAGE'] == 'true'
 	require 'simplecov'
+	require 'simplecov-lcov'
+	SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+
 	SimpleCov.start do
 		enable_coverage :branch
 		minimum_coverage line: 90, branch: 80
 		maximum_coverage_drop line: 5, branch: 10
+		formatter SimpleCov::Formatter::MultiFormatter.new([
+			SimpleCov::Formatter::SimpleFormatter,
+			SimpleCov::Formatter::LcovFormatter
+		])
 	end
 end
