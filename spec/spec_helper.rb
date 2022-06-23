81b60d5bfad4fce0a01b378f031b8d59065224c5
Gregorio Vazquez <gregoriovazya@gmail.com>
2022-06-23 16:32:00 -0600

    feat: update spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,6 @@
 if ENV['COVERAGE'] == 'true'
 	require 'simplecov'
 	require 'simplecov-lcov'
-	SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-	SimpleCov::Formatter::LcovFormatter.config do |c|
-    c.lcov_file_name = 'lcov.info' # default: "YOUR_PROJECT_NAME.lcov"
-  end
-
 	SimpleCov.start do
 		enable_coverage :branch
 		minimum_coverage line: 90, branch: 80
