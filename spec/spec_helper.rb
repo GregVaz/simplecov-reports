0c794e68e241d4734d68c400e934bdc15bb5a116
Gregorio Vazquez <gregoriovazya@gmail.com>
2022-06-23 14:33:04 -0600

    fix: update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 require 'simplecov-lcov'
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 SimpleCov.start 'rails' do
+	enable_coverage :branch
 	formatter SimpleCov::Formatter::MultiFormatter.new([
 		SimpleCov::Formatter::SimpleFormatter,
 		SimpleCov::Formatter::LcovFormatter
