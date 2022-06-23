916c0052202f5cf1319ef78451c9ee1bd95d23de
Gregorio Vazquez <gregoriovazya@gmail.com>
2022-06-23 14:20:42 -0600

    feat: spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require 'simplecov'
 require 'simplecov-lcov'
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
-SimpleCov.start
+SimpleCov.start 'rails' do
+	formatter SimpleCov::Formatter::MultiFormatter.new([
+		SimpleCov::Formatter::SimpleFormatter,
+		SimpleCov::Formatter::LcovFormatter
+	])
+end
